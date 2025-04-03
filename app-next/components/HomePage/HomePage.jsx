67deae6749a96aae5b886c87cf932c78bd1952ba
gayathri <gayathri.cake@gmail.com>
2025-04-03 12:14:49 +0200

    Add MealsList component to the home page

--- app-next/components/HomePage/HomePage.jsx
+++ app-next/components/HomePage/HomePage.jsx
@@ -1,18 +1,17 @@
+"use client";
+
 import HYFLogo from "@/assets/hyf.svg";
 import Image from "next/image";
-import "./HomePage.css";
+import styles from "./HomePage.module.css";
+import MealsList from "../MealsList/MealsList";
 
 // Feel free to replace the content of this component with your own
 function HomePage() {
   return (
-    <>
-      <a href="https://www.hackyourfuture.dk/" target="_blank" className="link">
-        <Image src={HYFLogo.src} width={HYFLogo.width} height={HYFLogo.height} className="logo" />
-      </a>
-      <a href="/nested" className="link">
-        <span className="message">Go to the nested page</span>
-      </a>
-    </>
+    <div>
+      <h1 className={styles.homeHeader}>Welcome to Meal Sharing</h1>
+      <MealsList />
+    </div>
   );
 }
 
